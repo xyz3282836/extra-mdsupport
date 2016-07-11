e78df86963d461bb7102b7fd22690a015cff8563
Zhou <ruizhouliu@163.com>
2016-07-11 19:16:44 +0800

    update

---
+++ support/src/main/java/com/rz/LoadRecyclerView/FooterNoMoreDataView.java
@@ -0,0 +1,18 @@
+package com.rz.LoadRecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.rz.R;
+
+/**
+ * Created by Zhou on 2016/6/29.
+ */
+public class FooterNoMoreDataView extends FootView {
+    @Override
+    public View onCreateView(ViewGroup parent) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.rv_recyclerview_nomoredata,parent,false);
+        return view;
+    }
+}
